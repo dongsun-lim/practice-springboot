61068a1bdd333dd6baab68f8392fa4e130542d38
dongsun.lim <dongsun.lim@thanks2u.tk>
2018-07-02 17:30:22 +0900

    Test get method

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Hello {
+
+    private int code;
+    private String msg;
+}
--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,38 +1,20 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by Dongsun Lim on 2018. 5. 18..
+ * Created by Dongsun Lim on 2018. 5. 29..
+ *
+ * http://easy-coding.tistory.com/1
  */
 @RestController
+@RequestMapping(value = "hello")
 public class HelloController {
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello, Spring Boot!";
+
+    @GetMapping
+    public Hello get() {
+        return new Hello(0, "Hello spring boot");
     }
-
-    @RequestMapping(value="hey1",method = RequestMethod.GET)
-    public String getMethod() {
-        return "get";
-    }
-
-    @RequestMapping(value="hey1", method = RequestMethod.PUT)
-    public String putMethod() {
-        return "put";
-    }
-
-    @RequestMapping(value = "/hey1", method = RequestMethod.POST)
-    public String postMethod1() {
-        return "post";
-    }
-
-    @RequestMapping(value = "/hey2", method = RequestMethod.POST)
-    public String postMethod2() {
-        return "hey post";
-    }
-
-
 }
